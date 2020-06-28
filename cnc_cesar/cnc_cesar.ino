ce161bbbd147adee4d3bbc835300c1e5533c97e8
Cesar Chavez <cesar.a.chavez.h@gmail.com>
2020-06-28 17:39:41 -0500

    comienzo

---
+++ cnc_cesar/cnc_cesar.ino
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
+  Serial.println("super CNC by Cesar");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
