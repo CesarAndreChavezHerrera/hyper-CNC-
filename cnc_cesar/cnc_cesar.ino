4539d0eba050b2af73c02740dfbc4cb393bef701
Cesar Chavez <cesar.a.chavez.h@gmail.com>
2020-06-28 18:49:01 -0500

    comienzo de la lectora

--- cnc_cesar/cnc_cesar.ino
+++ cnc_cesar/cnc_cesar.ino
@@ -1,11 +1,21 @@
 
+////////////////////////////////////////////
+//       variable de lectura de datos     //
+////////////////////////////////////////////
+const byte SPACE_DATA = 50;
+bool end_read_data = false;
 
-
-
+char serial_data_from_pc[SPACE_DATA];
 
 
 void setup() {
   // put your setup code here, to run once:
+  
+  
+  ////////////////////////////////////////////////
+  //             lectura de datos               //
+  ////////////////////////////////////////////////
+  
   Serial.begin(115200);
   delay(100);
   Serial.println("super CNC by Cesar");
@@ -13,5 +23,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+}
 
+
+  ////////////////////////////////////////////////
+  //             lectura de datos               //
+  ////////////////////////////////////////////////
+void _read_from_pc() {
+
+  byte count_data = 0;
+
+  // lee todo los datos que estan el bufet
+  if(Serial.available() > 0){
+
+    
+  }
 }
